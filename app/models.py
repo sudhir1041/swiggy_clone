1d39343dc5c21f9ebe02f412f3b59564fe0d3285
Sudhir Kumar <sudhir1041@gmail.com>
2024-08-18 21:18:44 +0530

    upadtes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Address(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    house_no=models.CharField(max_length=5)
+    area=models.CharField(max_length=100)
+    nearby=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    pin_code=models.IntegerField(max_length=6)
+    state=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user
